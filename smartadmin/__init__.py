93c562f691342a34f347fe995744109c251db725
Petr Pokorny <petr@innit.cz>
2013-06-21 15:15:09 +0200

    fixed possible installation issue

--- smartadmin/__init__.py
+++ smartadmin/__init__.py
@@ -4,5 +4,6 @@ __versionstr__ = '.'.join(map(str, VERSION))
 try:
     from smartadmin.admin import SmartAdmin, filter_existing
     # this only works inside a django project...
-except ImportError:
+except:
+    print "WARNING: Couldn't import smartadmin, try importing from smartadmin.admin to see why. Note that this only works from inside of a properly configured django project."
     pass
